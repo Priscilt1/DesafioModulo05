0e4140782ea1899a48466bb0644181a0f68f4c6a
Priscila <priscilt.ribeiro@hotmail.com>
2021-01-05 09:35:13 -0300

    criando models e aprofundando callback function

--- src/app/controllers/teachers.js
+++ src/app/controllers/teachers.js
@@ -1,13 +1,11 @@
-const { age, date, graduation } = require('../../lib/utils')
-const db = require('../../config/db')
 const Intl = require('intl')
+const Teacher = require('../models/Teacher')
 
 
 module.exports = {
     index(req, res) {
-        db.query(`SELECT * FROM teachers`, function(err, results){
-            if(err) return res.send("Erro no banco de dados!")
-            return res.render("teachers/index", {teachers: results.rows})
+        Teacher.all(function(teachers){
+            return res.render("teachers/index", {teachers})
         })
     },
     create(req, res) {
@@ -21,34 +19,9 @@ module.exports = {
             }
         }
     
-        const query = `
-            INSERT INTO teachers (
-                avatar_url,
-                name,
-                birth_date,
-                education_level,
-                class_type,
-                subjects_taught,
-                created_at
-            ) VALUES ($1, $2, $3, $4, $5, $6, $7)
-            RETURNING id
-        `
-
-        const values = [
-            req.body.avatar_url,
-            req.body.name,
-            date(req.body.birth_date).iso,
-            req.body.education_level,
-            req.body.class_type,
-            req.body.subjects_taught,
-            date(Date.now()).iso
-        ]
-        
-        db.query(query, values, function(err, results) {
-            if(err) return res.send("Erro no banco de dados!")
-            return res.redirect(`/instructors/${results.rows[0].id}`)
-        })
-
+        Teacher.create(req.body, function(teacher) {
+            return res.redirect(`/teachers/${teacher.id}`)
+        }) 
     },
     show(req, res) {
         return
