15a5a298b5f5a155f937ae999d7377d5e4befbda
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-01-06 10:24:41 -0300

    editando dados no banco

--- src/app/controllers/teachers.js
+++ src/app/controllers/teachers.js
@@ -28,18 +28,22 @@ module.exports = {
         Teacher.find(req.params.id, function (teacher) {
             if (!teacher) return res.send("Professor não encontrado!")
 
-            teacher.age = age(teacher.birth)
+            teacher.age = age(teacher.birth_date)
             teacher.subjects_taught = teacher.subjects_taught.split(",")
             teacher.created_at = date(teacher.created_at).format
             teacher.education_level = graduation(teacher.education_level)
 
             return res.render('teachers/show', {teacher})
         })
-
-        
     },
     edit(req, res) {
-        return
+        Teacher.find(req.params.id, function (teacher) {
+            if (!teacher) return res.send("Professor não encontrado!")
+
+            teacher.birth = date(teacher.birth).iso
+
+            return res.render('teachers/edit', {teacher})
+        })
     },
     put(req, res) {
         const keys = Object.keys(req.body)
@@ -48,7 +52,10 @@ module.exports = {
                 return res.send('Por favor, preencha todos os campos!')
             }
         }
-        return
+
+        Teacher.update(req.body, function(){
+            return res.redirect(`/teachers/${req.body.id}`)
+        })
     },
     delete(req, res) {
         return
--- src/app/models/Teacher.js
+++ src/app/models/Teacher.js
@@ -49,5 +49,34 @@ module.exports = {
                 if(err) throw `Erro no banco de dados!${err}`
                 callback(results.rows[0])
         })
+    },
+    // update = atualização/edicao do dado. O Where é muito importante para que nao atualize todos os dados, mas sim apenas o id indicado 
+    update(data, callback) {
+        const query = `
+            UPDATE teachers SET
+                avatar_url=($1),
+                name=($2),
+                birth_date=($3),
+                education_level=($4),
+                class_type=($5),
+                subjects_taught=($6)
+            WHERE id = $7
+        `
+
+        const values = [
+            data.avatar_url,
+            data.name,
+            date(data.birth_date).iso,
+            data.education_level,
+            data.class_type,
+            data.subjects_taught,
+            data.id
+        ]
+
+        db.query(query, values, function (err, results){
+            if(err) throw "Erro no banco de dados!"
+            callback()
+
+        })    
     }
 }
