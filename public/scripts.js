b141e21f1610c0e9347f4c4cd754b0ab730f883a
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-01-13 09:42:44 -0300

    finalização da logica de paginação

--- public/scripts.js
+++ public/scripts.js
@@ -9,17 +9,36 @@ for (item of menuItems) {
 }
 
 // Paginacao
-let totalPages = 20,
+let totalPages = 25,
     selectedPage = 15,
-    pages = []
+    pages = [],
+    oldPage
+
+    // 1, 2, ... 14, 15, 16, ... 24, 25 
 
 for(let currentPage =1; currentPage<= totalPages; currentPage++) {
     
-    const firstAndLastPage = currentPage == 1 || currentPage== totalPages
-    const pagesAfterSelectedPage = currentPage <= selectedPage +2
-    const pagesBeforeSelectedPage = currentPage >= selectedPage -2
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+    const secondPage = currentPage == 2 
+    const penultimatePage = currentPage == totalPages -1
+    const pagesAfterSelectedPage = currentPage <= selectedPage +1
+    const pagesBeforeSelectedPage = currentPage >= selectedPage -1
     
-    if(firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+    if(firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage || secondPage || penultimatePage) {
+        
+
+        if(oldPage && currentPage - oldPage >2) {
+            pages.push("...")
+        }
+
+        if(oldPage && currentPage - oldPage ==2) {
+            pages.push(oldPage + 2)
+        }
+        
         pages.push(currentPage)
+
+        oldPage = currentPage
     }
 }
+
+console.log(pages)
